2aa71b2beddf44313a3c46da9b11d3e79a9242eb
koekiemonster <arent23@hotmail.com>
2015-08-11 16:07:47 +0200

    second commit

---
+++ TempcollateTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `tempcollate` (
+  `tempcollate_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tempcollate_user` varbinary(255) NOT NULL,
+  `tempcollate_titles_array` varbinary(500) NOT NULL,
+  `tempcollate_new_url` varbinary(500) NOT NULL,
+  `tempcollate_main_title` varbinary(500) NOT NULL,
+  `tempcollate_main_title_lowercase` varbinary(500) NOT NULL,
+  `tempcollate_collatex` TEXT NOT NULL,
+  PRIMARY KEY (`tempcollate_id`),
+  UNIQUE KEY `tempcollate_user` (`tempcollate_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=binary;
