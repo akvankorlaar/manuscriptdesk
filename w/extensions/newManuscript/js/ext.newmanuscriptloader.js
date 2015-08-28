cfe7541ca257e7f0637512b8d2e4d6eeb8909a15
akvankorlaar <akvankorlaar@gmail.com>
2015-08-28 15:52:51 +0200

    changing newmanuscript javascript

--- w/extensions/newManuscript/js/ext.newmanuscriptloader.js
+++ w/extensions/newManuscript/js/ext.newmanuscriptloader.js
@@ -21,9 +21,15 @@
  * @copyright 2015 Arent van Korlaar
  */
 
+/**
+ * This file is mainly concerned with showing the loader gif, and javascript validation. There is also server side validation in SpecialBeginCollate,
+ * so the javascript validation is just to increase user-experience and reduce some irrelevant requests sent to the server
+ */
+
 (function (mw, $){
      
     /**
+     * 
      * 
      * @returns {undefined}
      */
@@ -34,11 +40,18 @@
       
       var file_input = $('#wpUploadFile').val();      
        
-       if(text_input1 != '' && text_input2 != '' && file_input != '' && text_input1.match(/[a-zA-Z0-9]/g) && text_input2.match(/[a-zA-Z0-9]/g)){         
-       //match does not work....... 
+       if(text_input1 != '' && file_input != '' ){         
+       //match does not work....... && text_input1.match(/[a-zA-Z0-9]/g) && text_input2.match(/[a-zA-Z0-9]/g)
+       //text_input2 is not required
          $(".mw-htmlform-submit").removeAttr("disabled");
-         $(".mw-htmlform-submit").css("cursor", "pointer");        
-       }
+         $(".mw-htmlform-submit").css("color", "black");
+         $(".mw-htmlform-submit").css("cursor", "pointer");
+         
+       }else{
+         $(".mw-htmlform-submit").attr("disabled","disabled");
+         $(".mw-htmlform-submit").css("color", "grey");
+         $(".mw-htmlform-submit").css("cursor", "default");
+      }         
     }
      
    /**
@@ -49,6 +62,13 @@
       $('h2').hide(); 
       $('.error').hide();
       $('#newmanuscript-loaderdiv').show();   
+    });
+    
+    /**
+     * This function disables the submit button when the document is ready 
+     */
+    $( document ).ready(function(){
+      $(".mw-htmlform-submit").attr("disabled","disabled");
     });
     
     //call the function checkValidation on change
