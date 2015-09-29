302a1d93b053e78bb2b2e8e4dc3df3e3e873c5be
akvankorlaar <akvankorlaar@gmail.com>
2015-09-29 10:22:09 +0200

    fix manuscriptsTable.sql

--- manuscriptsTable.sql
+++ manuscriptsTable.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS `manuscripts` (
   `manuscripts_date` varbinary(255) NOT NULL,
   `manuscripts_lowercase_title` varbinary(255) NOT NULL,
   `manuscripts_collection` varbinary(255) NOT NULL,
+  `manuscripts_lowercase_collection` varbinary(255) NOT NULL,
   `manuscripts_datesort` varbinary(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=binary;
