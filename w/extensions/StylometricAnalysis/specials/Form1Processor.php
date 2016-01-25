f3151dac7712bbcb0691b59c1ea5972f87410946
akvankorlaar <akvankorlaar@gmail.com>
2016-01-25 21:34:49 +0100

    format test

--- w/extensions/StylometricAnalysis/specials/Form1Processor.php
+++ w/extensions/StylometricAnalysis/specials/Form1Processor.php
@@ -1,67 +1,68 @@
 <?php
 
-class Form1Processor{
+class Form1Processor {
 
-  private $request;
-  private $validator; 
-	
-  public function __construct(Request $request, ManuscriptDeskBaseValidator $validator){
-	$this->request = $request;
-	$this->validator = $validator; 
-  }	
-  
-  /**
-   * This function processes form 1
-   */
-  public function processForm1(){	  
-	global $wgStylometricAnalysisOptions; 
-    $minimum_collections = $wgStylometricAnalysisOptions['wgmin_stylometricanalysis_collections'];  
-    $maximum_collections = $wgStylometricAnalysisOptions['wgmax_stylometricanalysis_collections'];
-    $collection_array = $this->loadForm1();
-    $this->checkForm1($collection_array, $minimum_collections, $maximum_collections);
-    return $collection_array;       
-  }
-  
-  /**
-   * This function loads the variables in Form 1
-   */
-  private function loadForm1(){
-      
-    $request = $this->request; 
-	$validator = $this->validator;
-    $posted_names = $request->getValueNames();
-	$collection_array = array();
-     
-    //identify the button pressed
-    foreach($posted_names as $key=>$checkbox){  
-      //remove the numbers from $checkbox to see if it matches to 'collection'
-      $checkbox_without_numbers = trim(str_replace(range(0,9),'',$checkbox));
+    private $request;
+    private $validator;
 
-      if($checkbox_without_numbers === 'collection'){
-        $collection_array[$checkbox] = (array)$validator->validateStringUrl(json_decode($request->getText($checkbox)));                      
-      }   
-    }
-     
-    return $collection_array;   
-  }
-  
-  /**
-   * This function checks form 1
-   */
-  private function checkForm1(array $collection_array, $minimum_collections, $maximum_collections){
-	  
-	if(empty($collection_array)){
-	  throw new Exception('stylometricanalysis-error-request');	
-	}  
-      
-    if(count($this->collection_array) < $minimum_collections){        
-      throw new Exception('stylometricanalysis-error-fewcollections');   
+    public function __construct(Request $request, ManuscriptDeskBaseValidator $validator) {
+        $this->request = $request;
+        $this->validator = $validator;
     }
 
-    if(count($this->collection_array) > $maximum_collections){
-      throw new Exception('stylometricanalysis-error-manycollections');   
+    /**
+     * This function processes form 1
+     */
+    public function processForm1() {
+        global $wgStylometricAnalysisOptions;
+        $minimum_collections = $wgStylometricAnalysisOptions['wgmin_stylometricanalysis_collections'];
+        $maximum_collections = $wgStylometricAnalysisOptions['wgmax_stylometricanalysis_collections'];
+        $collection_array = $this->loadForm1();
+        $this->checkForm1($collection_array, $minimum_collections, $maximum_collections);
+        return $collection_array;
     }
-    
-    return true; 
-  }
+
+    /**
+     * This function loads the variables in Form 1
+     */
+    private function loadForm1() {
+
+        $request = $this->request;
+        $validator = $this->validator;
+        $posted_names = $request->getValueNames();
+        $collection_array = array();
+
+        //identify the button pressed
+        foreach ($posted_names as $key => $checkbox) {
+            //remove the numbers from $checkbox to see if it matches to 'collection'
+            $checkbox_without_numbers = trim(str_replace(range(0, 9), '', $checkbox));
+
+            if ($checkbox_without_numbers === 'collection') {
+                $collection_array[$checkbox] = (array) $validator->validateStringUrl(json_decode($request->getText($checkbox)));
+            }
+        }
+
+        return $collection_array;
+    }
+
+    /**
+     * This function checks form 1
+     */
+    private function checkForm1(array $collection_array, $minimum_collections, $maximum_collections) {
+
+        if (empty($collection_array)) {
+            throw new Exception('stylometricanalysis-error-request');
+        }
+
+        if (count($this->collection_array) < $minimum_collections) {
+            throw new Exception('stylometricanalysis-error-fewcollections');
+        }
+
+        if (count($this->collection_array) > $maximum_collections) {
+            throw new Exception('stylometricanalysis-error-manycollections');
+        }
+
+        return true;
+    }
+
 }
--- w/extensions/StylometricAnalysis/specials/Form2Processor.php
+++ w/extensions/StylometricAnalysis/specials/Form2Processor.php
@@ -1,86 +1,85 @@
 <?php
 
-class Form2Processor{
-	
-  private $request;
-  private $validator; 
-  
-  public function __construct(Request $request, ManuscriptDeskBaseValidator $validator){
-	$this->request = $request;
-	$this->validator = $validator; 
-  }	
-	
-  /**
-   * This function processes form 2
-   */
-  public function processForm2(){
-	global $wgStylometricAnalysisOptions;   
-	$min_mfi = $wgStylometricAnalysisOptions['min_mfi'];     
-    $config_array = $this->loadForm2();
-    $this->checkForm2($config_array, $min_mfi);
-    return $config_array;       
-  }	
-  
-  /**
-   * This function loads the variables in Form 2
-   */
-  private function loadForm2(){
-      
-    $validator = $this->validator;   
-    $request = $this->request;
-	$config_array = array();
-                               
-    $config_array['removenonalpha'] = $request->getText('wpremovenonalpha');
-	$conofig_array['removenonalpha'] = empty($config_array['removenonalpha']) ? 0 : 1; 
+class Form2Processor {
 
-    $config_array['lowercase'] = $request->getText('wplowercase');
-	$config_array['lowercase'] = empty($config_array['lowercase']) ? 0 : 1;
+    private $request;
+    private $validator;
 
-    $config_array['tokenizer'] = $validator->validateString($request->getText('wptokenizer'));
-    $config_array['minimumsize'] = (int)$validator->validateNumber($request->getText('wpminimumsize'));
-    $config_array['maximumsize'] = (int)$validator->validateNumber($request->getText('wpmaximumsize'));
-    $config_array['segmentsize'] = (int)$validator->validateNumber($request->getText('wpsegmentsize'));
-    $config_array['stepsize'] = (int)$validator->validateNumber($request->getText('wpstepsize'));
-	
-    $config_array['removepronouns'] = $request->getText('wpremovepronouns');
-	$config_array['removepronouns'] = empty($config_array['removepronouns']) ? 0 : 1;
-
-    $config_array['vectorspace'] = $validator->validateString($request->getText('wpvectorspace'));
-    $config_array['featuretype'] = $validator->validateString($request->getText('wpfeaturetype'));
-
-    $config_array['ngramsize'] = (int)$validator->validateNumber($request->getText('wpngramsize'));
-    $config_array['mfi'] = (int)$validator->validateNumber($request->getText('wpmfi'));
-    $config_array['minimumdf'] = floatval($validator->validateNumber($request->getText('wpminimumdf')));
-    $config_array['maximumdf'] = floatval($validator->validateNumber($request->getText('wpmaximumdf')));
-
-    $config_array['visualization1'] = $validator->validateString($request->getText('wpvisualization1'));
-    $config_array['visualization2'] = $validator->validateString($request->getText('wpvisualization2'));
-
-    $config_array['collection_array'] = (array)$validator->validateStringUrl(json_decode($request->getText('collection_array')));
-
-    foreach($config_array['collection_array'] as $index=>&$value){
-      //cast everything in collection_array to an array
-      $config_array['collection_array'][$index] = (array)$value;
+    public function __construct(Request $request, ManuscriptDeskBaseValidator $validator) {
+        $this->request = $request;
+        $this->validator = $validator;
     }
-        
-    return $config_array; 
-  }
-  
-  /**
-   * This function checks form 2 
-   */
-  private function checkForm2($config_array, $min_mfi){
-              
-    if($config_array['minimumsize'] >= $config_array['maximumsize']){
-      throw new Exception('stylometricanalysis-error-minmax');   
+
+    /**
+     * This function processes form 2
+     */
+    public function processForm2() {
+        global $wgStylometricAnalysisOptions;
+        $min_mfi = $wgStylometricAnalysisOptions['min_mfi'];
+        $config_array = $this->loadForm2();
+        $this->checkForm2($config_array, $min_mfi);
+        return $config_array;
     }
-    
-    if($config_array['stepsize'] > $config_array['segmentsize']){
-      throw new Exception('stylometricanalysis-error-stepsizesegmentsize');   
+
+    /**
+     * This function loads the variables in Form 2
+     */
+    private function loadForm2() {
+        $validator = $this->validator;
+        $request = $this->request;
+        $config_array = array();
+
+        $config_array['removenonalpha'] = $request->getText('wpremovenonalpha');
+        $config_array['removenonalpha'] = empty($config_array['removenonalpha']) ? 0 : 1;
+
+        $config_array['lowercase'] = $request->getText('wplowercase');
+        $config_array['lowercase'] = empty($config_array['lowercase']) ? 0 : 1;
+
+        $config_array['tokenizer'] = $validator->validateString($request->getText('wptokenizer'));
+        $config_array['minimumsize'] = (int) $validator->validateNumber($request->getText('wpminimumsize'));
+        $config_array['maximumsize'] = (int) $validator->validateNumber($request->getText('wpmaximumsize'));
+        $config_array['segmentsize'] = (int) $validator->validateNumber($request->getText('wpsegmentsize'));
+        $config_array['stepsize'] = (int) $validator->validateNumber($request->getText('wpstepsize'));
+
+        $config_array['removepronouns'] = $request->getText('wpremovepronouns');
+        $config_array['removepronouns'] = empty($config_array['removepronouns']) ? 0 : 1;
+
+        $config_array['vectorspace'] = $validator->validateString($request->getText('wpvectorspace'));
+        $config_array['featuretype'] = $validator->validateString($request->getText('wpfeaturetype'));
+
+        $config_array['ngramsize'] = (int) $validator->validateNumber($request->getText('wpngramsize'));
+        $config_array['mfi'] = (int) $validator->validateNumber($request->getText('wpmfi'));
+        $config_array['minimumdf'] = floatval($validator->validateNumber($request->getText('wpminimumdf')));
+        $config_array['maximumdf'] = floatval($validator->validateNumber($request->getText('wpmaximumdf')));
+
+        $config_array['visualization1'] = $validator->validateString($request->getText('wpvisualization1'));
+        $config_array['visualization2'] = $validator->validateString($request->getText('wpvisualization2'));
+
+        $config_array['collection_array'] = (array) $validator->validateStringUrl(json_decode($request->getText('collection_array')));
+
+        foreach ($config_array['collection_array'] as $index => &$value) {
+            //cast everything in collection_array to an array
+            $config_array['collection_array'][$index] = (array) $value;
+        }
+
+        return $config_array;
     }
-    
-    if($config_array['mfi'] < $min_mfi){
-      throw new Exception('stylometricanalysis-error-mfi');   
-    } 
-  }
+
+    /**
+     * This function checks form 2 
+     */
+    private function checkForm2($config_array, $min_mfi) {
+
+        if ($config_array['minimumsize'] >= $config_array['maximumsize']) {
+            throw new Exception('stylometricanalysis-error-minmax');
+        }
+
+        if ($config_array['stepsize'] > $config_array['segmentsize']) {
+            throw new Exception('stylometricanalysis-error-stepsizesegmentsize');
+        }
+
+        if ($config_array['mfi'] < $min_mfi) {
+            throw new Exception('stylometricanalysis-error-mfi');
+        }
+    }
 }
