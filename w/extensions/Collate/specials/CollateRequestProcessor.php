b4a89b2f7719e4e302d2f1703fb8526506dfad3b
Arent van Korlaar <akvankorlaar@gmail.com>
2016-05-17 16:03:19 +0200

    changed collate signature

--- w/extensions/Collate/specials/CollateRequestProcessor.php
+++ w/extensions/Collate/specials/CollateRequestProcessor.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * This file is part of the Collate extension
- * Copyright (C) 2015 Arent van Korlaar
+ * This file is part of the Manuscript Desk (github.com/akvankorlaar/manuscriptdesk)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
