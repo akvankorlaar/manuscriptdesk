83a6e1cc753ce50c0febd11fd78f606dff8b238f
Arent van Korlaar <akvankorlaar@gmail.com>
2016-06-01 14:04:33 +0200

    cleanup test stylometricanalysis

--- w/extensions/StylometricAnalysis/specials/SpecialStylometricAnalysis.php
+++ w/extensions/StylometricAnalysis/specials/SpecialStylometricAnalysis.php
@@ -399,7 +399,7 @@ class SpecialStylometricAnalysis extends ManuscriptDeskBaseSpecials {
         }
 
         if (!is_file($full_outputpath1) || !is_file($full_outputpath2)) {
-            throw new \Exception($pystyl_output);
+            throw new \Exception('stylometricanalysis-error-internal');
         }
 
         return true;
